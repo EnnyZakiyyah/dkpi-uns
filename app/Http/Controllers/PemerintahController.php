24a0474b87a5c4a917d7571a0b24b91861b96299
armadamuhammad <armada_muhammad@student.uns.ac.id>
2021-10-28 14:46:23 +0700

    nambahin model dan controller

---
+++ app/Http/Controllers/PemerintahController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pemerintah;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PemerintahController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pemerintah  $pemerintah
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pemerintah $pemerintah)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pemerintah  $pemerintah
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pemerintah $pemerintah)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pemerintah  $pemerintah
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pemerintah $pemerintah)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pemerintah  $pemerintah
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pemerintah $pemerintah)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/SekolahController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sekolah;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SekolahController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Sekolah  $sekolah
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Sekolah $sekolah)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Sekolah  $sekolah
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Sekolah $sekolah)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Sekolah  $sekolah
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Sekolah $sekolah)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Sekolah  $sekolah
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Sekolah $sekolah)
+    {
+        //
+    }
+}
