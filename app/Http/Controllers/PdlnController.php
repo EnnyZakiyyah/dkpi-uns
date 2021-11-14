7aa1b39404b0a308df0e9e09d7dbe5e90ca247eb
armadamuhammad <armada_muhammad@student.uns.ac.id>
2021-11-15 06:39:13 +0700

    add pdln controller

---
+++ app/Http/Controllers/PdlnController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pdln;
+use Illuminate\Http\Request;
+
+class PdlnController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pdln  $pdln
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pdln $pdln)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pdln  $pdln
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pdln $pdln)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pdln  $pdln
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pdln $pdln)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pdln  $pdln
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pdln $pdln)
+    {
+        //
+    }
+}
