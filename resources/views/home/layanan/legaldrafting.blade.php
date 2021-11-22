69443a7076b2a1f949e2f9206f685d716561d85e
EnnyZakiyyah <enny.9h08@gmail.com>
2021-11-22 15:38:33 +0700

    -M: Init repair lagaldrafting

--- resources/views/home/layanan/legaldrafting.blade.php
+++ resources/views/home/layanan/legaldrafting.blade.php
@@ -14,62 +14,107 @@
             {{-- <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p> --}}
           </div>
 
+          <h2><span class="badge mb-3 pl-2" style="background-color: rgba(190, 35, 121, 0.75)">SOP</span></h2>
           <div class="container">
             <div class="row">
               <div class="col-1">
                 <ol class="list-group list-group-numbered">
-                  <h3><span class="badge bg-secondary">1</span></h3>
+                  <h3><span class="badge" style="background-color: rgba(108, 88, 179, 0.75)">1</span></h3>
                 </ol>
               </div>
-              <div class="col-11 mb-4" style="align-left">Inisiasi MoU oleh pihak eksternal atau unit fakultas</div>
+              <div class="col-11 mb-4" style="align-left">Inisiasi MoU sebagai payung kerjasama dapat dilakukan oleh Pihak eksternal atau Unit/ Fakultas dengan lebih dahulu merencanakan program/kegiatan konkret yang akan dilakukan oleh Unit/Lembaga/Fakultas/Prodi dengan pihak mitra yang nantinya akan diwujudkan dalam bentuk Perjanjian Kerjasama/kontrak kerja/kontrak swakelola/penunjukan langsung.</div>
              
               <div class="col-1">
                 <ol class="list-group list-group-numbered">
-                  <h3><span class="badge bg-secondary">2</span></h3>
+                  <h3><span class="badge" style="background-color: rgba(108, 88, 179, 0.75)">2</span></h3>
                 </ol>
               </div>
-              <div class="col-11 mb-4  style="align-left">Mengajukan surat permohonan kerjasama kepada Wakil Rektor Bidang Perencanaan, Kerjasama, Bisnis, dan Informasi (WR IV) yang dikirimkan ke Direktorat Kerjasama, Pengembangan, dan Internasionalisasi (DKPI) dengan melampiri Draf MoU dan rencana program/kegiatan yang hendak dilaksanakan.</div>
+              <div class="col-11 mb-4  style="align-left">Pihak eksternal atau Unit/Lembaga/Fakultas di lingkungan UNS dapat mengajukan surat permohonan kerjasama kepada Wakil Rektor Bidang Perencanaan, Kerjasama, Bisnis, dan Informasi (WR IV) yang dikirimkan ke Direktorat Kerjasama, Pengembangan, dan Internasionalisasi (DKPI).</div>
             </div>
             <div class="row">
               <div class="col-1">
                 <ol class="list-group list-group-numbered">
-                  <h3><span class="badge bg-secondary">3</span></h3>
+                  <h3><span class="badge" style="background-color: rgba(108, 88, 179, 0.75)">3</span></h3>
                 </ol>
               </div>
-              <div class="col-11 mb-4" style="align-left">DKPI akan memproses dengan mereview/menyusun draf MoU dan mengkomunikasikannya kepada PIC Unit/Lembaga/Fakultas di lingkungan UNS atau langsung kepada pihak eksternal.</div>
+              <div class="col-11 mb-4" style="align-left">
+                Surat Permohonan Kerjasama dari Pihak eksternal atau Unit/Lembaga/Fakultas di lingkungan UNS dilampiri dengan : <br/><br/>
+                <i class="ri-check-double-line" style="color : rgba(190, 35, 121, 0.75)"></i> Draf MoU apabila sudah ada dalam bentuk hardcopy dan softcopy yang dikirimkan melalui sikd. Apabila belum ada draf yang akan di-review-kan maka  DKPI yang akan menyusun draf MoU tersebut;</li><br/>
+                <i class="ri-check-double-line" style = "color : rgba(190, 35, 121, 0.75)"></i> Rencana program/kegiatan yang hendak dilaksanakan.</li>
+               
+              </div>
              
               <div class="col-1">
                 <ol class="list-group list-group-numbered">
-                  <h3><span class="badge bg-secondary">4</span></h3>
+                  <h3><span class="badge" style="background-color: rgba(108, 88, 179, 0.75)">4</span></h3>
                 </ol>
               </div>
-              <div class="col-11 mb-4  style="align-left">Draf MoU akan dicetak dalam bentuk draf dan dinaikkan kepada Pimpinan UNS untuk review terakhir sebelum dicetak final di kertas standar MoU.</div>
+              <div class="col-11 mb-4  style="align-left">DKPI akan menindaklanjuti permohonan kerjasama tersebut dan memproses secara legal drafting dengan mereview/menyusun draf MoU dan mengkomunikasikannya kepada PIC Unit/Lembaga/Fakultas di lingkungan UNS atau langsung kepada pihak eksternal.</div>
             </div>
             <div class="row">
               <div class="col-1">
                 <ol class="list-group list-group-numbered">
-                  <h3><span class="badge bg-secondary">5</span></h3>
+                  <h3><span class="badge" style="background-color: rgba(108, 88, 179, 0.75)">5</span></h3>
                 </ol>
               </div>
-              <div class="col-11 mb-4" style="align-left">Draf MoU yang telah final akan dicetak jadi, diberi materai dan dinaikkan untuk tanda tangan Rektor atau Wakil Rektor Perencanaan, Kerjasama, Bisnis, dan Informasi dengan lebih dahulu diparaf oleh Koor. KPI dan Direktur KPI.</div>
+              <div class="col-11 mb-4" style="align-left">Draf MoU akan dicetak dalam bentuk draf dan dinaikkan kepada Pimpinan UNS untuk review terakhir sebelum dicetak final di kertas standar MoU.</div>
              
               <div class="col-1">
                 <ol class="list-group list-group-numbered">
-                  <h3><span class="badge bg-secondary">6</span></h3>
+                  <h3><span class="badge" style="background-color: rgba(108, 88, 179, 0.75)">6</span></h3>
                 </ol>
               </div>
-              <div class="col-11 mb-4  style="align-left">Pemberian Nomor khusus MoU dan stempel dilakukan oleh DKPI.</div>
+              <div class="col-11 mb-4  style="align-left">Draf MoU yang telah final akan dicetak jadi, diberi materai dan dinaikkan untuk tanda tangan Rektor atau Wakil Rektor Perencanaan, Kerjasama, Bisnis, dan Informasi dengan lebih dahulu diparaf oleh Koor. KPI dan Direktur KPI.</div>
             </div>
             <div class="row">
               <div class="col-1">
                 <ol class="list-group list-group-numbered">
-                  <h3><span class="badge bg-secondary">7</span></h3>
+                  <h3><span class="badge" style="background-color: rgba(108, 88, 179, 0.75)">7</span></h3>
+                </ol>
+              </div>
+              <div class="col-11 mb-4" style="align-left">Pemberian Nomor khusus MoU dan stempel dilakukan oleh DKPI.
+              </div>
+          </div>
+            <div class="row">
+              <div class="col-1">
+                <ol class="list-group list-group-numbered">
+                  <h3><span class="badge" style="background-color: rgba(108, 88, 179, 0.75)">8</span></h3>
                 </ol>
               </div>
               <div class="col-11 mb-4" style="align-left">MoU yang telah jadi dengan materai di pihak UNS dikirimkan kepada pihak mitra.
               </div>
           </div>
           <br>
+          <h2><span class="badge mb-3 pl-2" style="background-color: rgba(190, 35, 121, 0.75)">PERSYARATAN</span></h2>
+          <div class="container">
+            <div class="row">
+              <div class="col-1">
+                <ol class="list-group list-group-numbered">
+                  <h3><span class="badge" style="background-color: rgba(108, 88, 179, 0.75)">1</span></h3>
+                </ol>
+              </div>
+              <div class="col-11 mb-4" style="align-left">MoU/ Nota Kesepahaman/ Nota Kesepahaman Bersama/ Nota Kesepakatan/ Nota Kesepakatan Bersama (bisa menggunakan salah satu istilah)</div>
+             
+              <div class="col-1">
+                <ol class="list-group list-group-numbered">
+                  <h3><span class="badge" style="background-color: rgba(108, 88, 179, 0.75)">2</span></h3>
+                </ol>
+              </div>
+              <div class="col-11 mb-4  style="align-left">Pihak-pihak yang melakukan kerjasama</div>
+              <div class="col-1">
+                <ol class="list-group list-group-numbered">
+                  <h3><span class="badge" style="background-color: rgba(108, 88, 179, 0.75)">3</span></h3>
+                </ol>
+              </div>
+              <div class="col-11 mb-4  style="align-left">Nomor MoU para pihak</div>
+              <div class="col-1">
+                <ol class="list-group list-group-numbered">
+                  <h3><span class="badge" style="background-color: rgba(108, 88, 179, 0.75)">4</span></h3>
+                </ol>
+              </div>
+              <div class="col-11 mb-4  style="align-left">Perihal yang dikerjasamakan adalah Penyelenggaraan Tri Dharma Perguruan Tinggi dan Penyelenggaraan Merdeka Belajar – Kampus Merdeka </div>
+            </div>
+          </div>
           <center><div class="d-grid gap-5 d-md-block">
             <a class="btn btn-secondary" type="button" href="/layanan">Kembali</a>
             <button class="btn btn-primary" type="button"><i class="bi bi-download">&nbsp; Download File</i></button>
