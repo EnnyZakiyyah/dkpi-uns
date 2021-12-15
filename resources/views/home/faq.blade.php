0ae64faeabaa30a80f9fe51f6d6d92462e309fa6
Armada Muhammad <81207935+armadamuhammad@users.noreply.github.com>
2021-12-15 08:33:37 +0700

    fixing routes

--- resources/views/home/faq.blade.php
+++ resources/views/home/faq.blade.php
@@ -20,7 +20,7 @@
 
 
           <li data-aos="fade-up" data-aos-delay="{{ $i = $loop->iteration }}00">
-            <i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" class="collapse" data-bs-target="#faq-list-{{ $i }}">{{ $faq->pertanyaan }}<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+        <i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" class="collapse" data-bs-target="#faq-list-{{ $i }}">{!! $faq->pertanyaan !!}<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
             <div id="faq-list-{{ $i }}" class="collapse show" data-bs-parent=".faq-list">
               <p>{{ $faq->jawaban }}</p>
             </div>
