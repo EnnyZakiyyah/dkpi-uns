d1532af5e5e7e8780b9f3fe8c4a7feb14b3f3369
EnnyZakiyyah <enny.9h08@gmail.com>
2021-12-24 10:24:23 +0700

    repair ui galeri

--- resources/views/home/galeri.blade.php
+++ resources/views/home/galeri.blade.php
@@ -19,20 +19,20 @@
               <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
               <!-- CV/PT -->
               <div class="col-lg-6 col-md-6 portfolio-item filter-mou">
-                <div class="portfolio-img"><iframe width="560" height="315" src="https://www.youtube.com/embed/YHyO-N8OkSQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                <div class="portfolio-img"><iframe width="650" height="370" src="https://www.youtube.com/embed/YHyO-N8OkSQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
                 <div class="portfolio-info">
                   <h4>Penandatanganan MoU Universitas Sebelas Maret</h4>
                   <p>Bank Syariah Indonesia</p>
-                  <a href="https://www.youtube.com/embed/YHyO-N8OkSQ" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>
+                  <a href="https://www.youtube.com/embed/YHyO-N8OkSQ" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Penandatanganan MoU Universitas Sebelas Maret"><i class="bx bx-plus"></i></a>
                   <a href="/galeri/more-details" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                 </div>
               </div>
               <div class="col-lg-6 col-md-6 portfolio-item filter-mou">
-                <div class="portfolio-img"><iframe width="560" height="315" src="https://www.youtube.com/embed/QvqNUeE7A74" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                <div class="portfolio-img"><iframe width="650" height="370" src="https://www.youtube.com/embed/QvqNUeE7A74" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
                 <div class="portfolio-info">
                   <h4>Penandatanganan MoU Universitas Sebelas Maret</h4>
-                  <p>Bank Syariah Indonesia</p>
-                  <a href="https://www.youtube.com/embed/QvqNUeE7A74" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>
+                  <p>University of Panama</p>
+                  <a href="https://www.youtube.com/embed/QvqNUeE7A74" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Penandatanganan MoU Universitas Sebelas Maret"><i class="bx bx-plus"></i></a>
                   <a href="/galeri/more-details" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                 </div>
               </div>
