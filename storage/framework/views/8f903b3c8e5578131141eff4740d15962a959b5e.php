f215aabf3b86cda23bf2c7316f7b245edeec7e87
ha-bib <81207935+armadamuhammad@users.noreply.github.com>
2021-12-26 14:30:34 +0700

    composer

--- storage/framework/views/8f903b3c8e5578131141eff4740d15962a959b5e.php
+++ storage/framework/views/8f903b3c8e5578131141eff4740d15962a959b5e.php
@@ -27,8 +27,6 @@
   <link href="<?php echo e(asset('/images/uns-touch-icon.png')); ?>" rel="apple-touch-icon">
   <!-- Fonts -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
-  <!-- DatePicker -->
-  <script src="<?php echo e(asset('assets2/vendor/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')); ?>"></script>
   <!-- Icons -->
   <link rel="stylesheet" href="<?php echo e(asset('assets2/vendor/nucleo/css/nucleo.css')); ?>" type="text/css">
   <link rel="stylesheet" href="<?php echo e(asset('assets2/vendor/@fortawesome/fontawesome-free/css/all.min.css')); ?>" type="text/css">
@@ -61,69 +59,82 @@
                 <!-- Nav items -->
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link <?php echo e(($title === "Dashboard") ? 'active' : ''); ?> " href="<?php echo e(url('/dashboard')); ?>">
+                        <a class="nav-link active" href="<?php echo e(url('/dashboard')); ?>">
                             <i class="ni ni-tv-2 text-primary"></i>
                             <span class="nav-link-text">Dashboard</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link <?php echo e(($title === "Data PDLN") ? 'active' : ''); ?>" href="<?php echo e(url('/pdln')); ?>">
-                            <i class="ni ni-archive-2 text-warning"></i>
+                        <a class="nav-link active" href="<?php echo e(url('/pdln')); ?>">
+                            <i class="ni ni-tv-2 text-primary"></i>
                             <span class="nav-link-text">Data PDLN</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link <?php echo e(($title === "Data Mitra") ? 'active' : ''); ?>" href="<?php echo e(url('/mitra')); ?>">
-                            <i class="ni ni-folder-17 text-info"></i>
+                        <a class="nav-link active" href="<?php echo e(url('/mitra')); ?>">
+                            <i class="ni ni-tv-2 text-primary"></i>
                             <span class="nav-link-text">Data mitra</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link <?php echo e(($title === "Pengumuman") ? 'active' : ''); ?>" href="<?php echo e(url('/pengumuman')); ?>">
-                            <i class="ni ni-notification-70 text-red"></i>
-                            <span class="nav-link-text">Pengumuman</span>
+                        <a class="nav-link active" href="<?php echo e(url('/pengumuman')); ?>">
+                            <i class="ni ni-tv-2 text-primary"></i>
+                            <span class="nav-link-text">pengumuman</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link <?php echo e(($title === "Berita") ? 'active' : ''); ?>" href="<?php echo e(url('/berita')); ?>">
-                            <i class="ni ni-books text-orange"></i>
-                            <span class="nav-link-text">Berita</span>
+                        <a class="nav-link active" href="<?php echo e(url('/berita')); ?>">
+                            <i class="ni ni-tv-2 text-primary"></i>
+                            <span class="nav-link-text">berita</span>
                         </a>
                     </li>
                     <li class="nav-item">
+<<<<<<< Updated upstream
+                        <a class="nav-link active" href="<?php echo e(url('/galeri')); ?>">
+                            <i class="ni ni-tv-2 text-primary"></i>
+                            <span class="nav-link-text">galeri</span>
+=======
                         <a class="nav-link <?php echo e(($title === "Galeri") ? 'active' : ''); ?>" href="<?php echo e(url('/gallery')); ?>">
                             <i class="ni ni-image text-primary"></i>
                             <span class="nav-link-text">Galeri</span>
+>>>>>>> Stashed changes
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link <?php echo e(($title === "Peringkat") ? 'active' : ''); ?>" href="<?php echo e(url('/peringkat')); ?>">
-                            <i class="ni ni-trophy text-yellow"></i>
-                            <span class="nav-link-text">Peringkat</span>
+                        <a class="nav-link active" href="<?php echo e(url('/peringkat')); ?>">
+                            <i class="ni ni-tv-2 text-primary"></i>
+                            <span class="nav-link-text">peringkat</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link <?php echo e(($title === "Pengaduan") ? 'active' : ''); ?>" href="<?php echo e(url('/pengaduan')); ?>">
-                            <i class="ni ni-send text-primary"></i>
-                            <span class="nav-link-text">Pengaduan</span>
+                        <a class="nav-link active" href="<?php echo e(url('/pengaduan')); ?>">
+                            <i class="ni ni-tv-2 text-primary"></i>
+                            <span class="nav-link-text">pengaduan</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link <?php echo e(($title === "FAQ") ? 'active' : ''); ?>" href="<?php echo e(url('/faq')); ?>">
-                            <i class="ni ni-chat-round text-success"></i>
+                        <a class="nav-link active" href="<?php echo e(url('/faq')); ?>">
+                            <i class="ni ni-tv-2 text-primary"></i>
                             <span class="nav-link-text">FAQ</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link <?php echo e(($title === "Register Account") ? 'active' : ''); ?>" href="<?php echo e(url('/register')); ?>">
-                            <i class="ni ni-circle-08 text-pink"></i>
-                            <span class="nav-link-text">Register Account</span>
+                        <a class="nav-link active" href="<?php echo e(url('/register')); ?>">
+                            <i class="ni ni-tv-2 text-primary"></i>
+                            <span class="nav-link-text">Register account</span>
                         </a>
                     </li>
                 </ul>
                 <!-- Divider -->
                 <hr class="my-3">
+<<<<<<< Updated upstream
+                <!-- Heading -->
+                <h6 class="navbar-heading p-0 text-muted">
+                    <span class="docs-normal">Documentation</span>
+                </h6>
+=======
 
+>>>>>>> Stashed changes
             </div>
         </div>
     </div>
@@ -136,7 +147,19 @@
       <div class="container-fluid">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <!-- Search form -->
-         <?php echo $__env->yieldContent('search'); ?>
+          <form class="navbar-search navbar-search-light form-inline mr-sm-3" id="navbar-search-main">
+            <div class="form-group mb-0">
+              <div class="input-group input-group-alternative input-group-merge">
+                <div class="input-group-prepend">
+                  <span class="input-group-text"><i class="fas fa-search"></i></span>
+                </div>
+                <input class="form-control" placeholder="Search" type="text">
+              </div>
+            </div>
+            <button type="button" class="close" data-action="search-close" data-target="#navbar-search-main" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </form>
           <!-- Navbar links -->
           <ul class="navbar-nav align-items-center  ml-md-auto ">
             <li class="nav-item d-xl-none">
@@ -149,13 +172,89 @@
                 </div>
               </div>
             </li>
+<<<<<<< Updated upstream
+            <li class="nav-item d-sm-none">
+              <a class="nav-link" href="#" data-action="search-show" data-target="#navbar-search-main">
+                <i class="ni ni-zoom-split-in"></i>
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="ni ni-bell-55"></i>
+              </a>
+              <div class="dropdown-menu dropdown-menu-xl  dropdown-menu-right  py-0 overflow-hidden">
+                <!-- Dropdown header -->
+                <div class="px-3 py-3">
+                  <h6 class="text-sm text-muted m-0">You have <strong class="text-primary">13</strong> notifications.</h6>
+                </div>
+                <!-- List group -->
+
+                        <!-- Avatar -->
+                        
+                      </div>
+
+                <!-- View all -->
+                <a href="#!" class="dropdown-item text-center text-primary font-weight-bold py-3">View all</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="ni ni-ungroup"></i>
+              </a>
+              <div class="dropdown-menu dropdown-menu-lg dropdown-menu-dark bg-default  dropdown-menu-right ">
+                <div class="row shortcuts px-4">
+                  <a href="#!" class="col-4 shortcut-item">
+                    <span class="shortcut-media avatar rounded-circle bg-gradient-red">
+                      <i class="ni ni-calendar-grid-58"></i>
+                    </span>
+                    <small>Calendar</small>
+                  </a>
+                  <a href="#!" class="col-4 shortcut-item">
+                    <span class="shortcut-media avatar rounded-circle bg-gradient-orange">
+                      <i class="ni ni-email-83"></i>
+                    </span>
+                    <small>Email</small>
+                  </a>
+                  <a href="#!" class="col-4 shortcut-item">
+                    <span class="shortcut-media avatar rounded-circle bg-gradient-info">
+                      <i class="ni ni-credit-card"></i>
+                    </span>
+                    <small>Payments</small>
+                  </a>
+                  <a href="#!" class="col-4 shortcut-item">
+                    <span class="shortcut-media avatar rounded-circle bg-gradient-green">
+                      <i class="ni ni-books"></i>
+                    </span>
+                    <small>Reports</small>
+                  </a>
+                  <a href="#!" class="col-4 shortcut-item">
+                    <span class="shortcut-media avatar rounded-circle bg-gradient-purple">
+                      <i class="ni ni-pin-3"></i>
+                    </span>
+                    <small>Maps</small>
+                  </a>
+                  <a href="#!" class="col-4 shortcut-item">
+                    <span class="shortcut-media avatar rounded-circle bg-gradient-yellow">
+                      <i class="ni ni-basket"></i>
+                    </span>
+                    <small>Shop</small>
+                  </a>
+                </div>
+              </div>
+            </li>
+=======
 
 
+>>>>>>> Stashed changes
           </ul>
           <ul class="navbar-nav align-items-center  ml-auto ml-md-0 ">
             <li class="nav-item dropdown">
               <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <div class="media align-items-center">
+<<<<<<< Updated upstream
+
+                  <div class="media-body  ml-2  d-none d-lg-block">
+=======
                   <div class="row align-items-center">
                     <div class="col-auto">
                       <!-- Avatar -->
@@ -171,27 +270,13 @@
                     </div>
                   </div>
                   
-              </a>
-              <div class="dropdown-menu  dropdown-menu-right ">
-                <div class="dropdown-header noti-title">
-                  <h6 class="text-overflow m-0">Welcome!</h6>
-                </div>
-
-                <div class="dropdown-divider"></div>
-
-                <form action="/logout" method="POST" class="dropdown-item">
-                    <?php echo csrf_field(); ?>
-                    <button type="submit" class="btn btn-danger">logout</button>
-                </form>
-              </div>
-            
           
         
       
     </nav>
    <?php echo $__env->yieldContent('container'); ?>
       <!-- Footer -->
-      <footer class="footer pt-5">
+      <footer class="footer pt-0">
         <div class="row align-items-center justify-content-lg-between">
           <div class="col-lg-6">
             <div class="copyright text-center  text-lg-left  text-muted">
